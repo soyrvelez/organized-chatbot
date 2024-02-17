7a165b310f83a03417501b5e26a0da4b2c18fac4
Rodrigo Vélez <Velez59@gmail.com>
2024-02-17 15:41:24 -0800

    feat: login button session recognition test

--- components/header.tsx
+++ components/header.tsx
@@ -47,7 +47,7 @@ async function UserOrLogin() {
   )
 }
 
-export function Header() {
+export async function Header() {
   const session = await auth()
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
@@ -57,7 +57,9 @@ export function Header() {
         </React.Suspense>
       </div>
       <div className="flex items-center justify-end space-x-2">
-      {!session?.user && <LoginButton />}
+      {session?.user ? (
+          <LoginButton />
+        ) : ("")}
         <a
           target="_blank"
           href="https://github.com/soyrvelez/organized-chatbot"
