dd2673260a47fbcb6c87df09fbfbbdebbd15e2d4
Rodrigo Vélez <Velez59@gmail.com>
2024-02-18 12:10:58 -0800

    feat: preferences post route

--- app/api/preferences/route.ts
+++ app/api/preferences/route.ts
@@ -2,8 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const runtime = 'edge';
-
 // View A Preference
 // View all preferences associated with users
 // View all preferences by model
