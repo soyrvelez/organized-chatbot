eadcbea605620128711b389848fbd88de97ef018
Rodrigo Vélez <Velez59@gmail.com>
2024-02-18 08:07:08 -0800

    chore: updated callback url

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -25,7 +25,7 @@ export function LoginButton({
       onClick={() => {
         setIsLoading(true)
         // next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
-        signIn('github', { callbackUrl: `/` })
+        signIn('github', { callbackUrl: `https://organized-chatbot.vercel.app/auth/github` })
       }}
       disabled={isLoading}
       className={cn(className)}
