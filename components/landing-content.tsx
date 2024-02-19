4984083849c43bce3d76946ed8dcb318a449adc1
Rodrigo Vélez <Velez59@gmail.com>
2024-02-19 10:56:58 -0800

    chore: landing content branding update

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,16 +1,18 @@
 'use client'
 import * as React from 'react'
 import { cn } from '@/lib/utils'
+import { Badge } from './ui/badge';
 
 export function LandingContent() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center text-center">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Organized Chatbot
       </h1>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         An amazing and beautiful application.
       </p>
+      <Badge variant={'default'}>Powered by Next.js' App Router</Badge>
     </div>
   );
 }
