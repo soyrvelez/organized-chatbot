baa367371c1a2149be1bed98d87cdcb84f8f4b3f
Rodrigo Vélez <Velez59@gmail.com>
2024-02-19 09:31:51 -0800

    feat: init profile component

---
+++ app/(profile)/page.tsx
@@ -0,0 +1,12 @@
+import ChatLayout from '../(chat)/layout'
+import Profile from '@/components/profile'
+
+const ProfilePage = () => {
+  return (
+    <ChatLayout>
+      <Profile />
+    </ChatLayout>
+  )
+}
+
+export default ProfilePage
